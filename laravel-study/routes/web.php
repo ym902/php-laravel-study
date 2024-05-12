c797be46acdc9d3665c51ac7350a040c0be8e2f3
yuukimochizuki <y.mochizuki.92@gmail.com>
2024-05-12 23:48:30 +0900

    Laravel ファイル等の初期設定

--- laravel-study/routes/web.php
+++ laravel-study/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('about', [HelloController::class, 'index']);
