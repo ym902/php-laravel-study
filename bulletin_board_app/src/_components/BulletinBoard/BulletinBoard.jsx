7fa82c71152daa0b0327a77ae9ad0584b1f718d4
yuukimochizuki <y.mochizuki.92@gmail.com>
2024-05-14 02:25:06 +0900

    マイグレーション完了

--- bulletin_board_app/src/_components/BulletinBoard/BulletinBoard.jsx
+++ bulletin_board_app/src/_components/BulletinBoard/BulletinBoard.jsx
@@ -1,6 +1,18 @@
 import './bulletinBoard.css';
 
 function BulletinBoard() {
+  // <?php
+  //   // スーパーグローバル変数 連想配列
+  //   echo $_POST["submitButton"]; // value属性が読み込まれる
+
+  //   if(!empty($_POST["submitButton"])) {
+  //     // empty に値が入っているときの処理
+  //     echo $_POST["username"];
+  //     echo $_POST["comment"];
+  //   }
+  // ?>
+
+
   return (
     <>
     <div>
@@ -23,15 +35,15 @@ function BulletinBoard() {
         </section>
 
         {/* 書き込みフォーム */}
-        <form className='form-wrapper'>
+        <form className='form-wrapper' method='POST'>
           <div>
-            <input type="submit" value="書き込む" />
+            <input type="submit" value="書き込む" name='submitButton' />
             <label htmlFor="">名前：</label>
             <input type="text" name="username" />
           </div>
 
           <div>
-            <textarea className='comment-text-area'></textarea>
+            <textarea className='comment-text-area' name='comment'></textarea>
           </div>
         </form>
       </div>
---
+++ laravel-study/database/migrations/2024_05_13_154955_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+// Schemaファサード、Blueprintオブジェクトが読み込まれている
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // postsテーブルがなければ作成する処理
+        if (!Schema::hasTable('posts')) {
+            Schema::create('posts', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('username', 30);
+                $table->text('comment');
+                $table->date('postDate');
+                // $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
