7f134b7d50c7e9e117307efe5829adf6eb99d295
yuukimochizuki <y.mochizuki.92@gmail.com>
2024-05-16 23:24:03 +0900

    IDパラメータでDBからデータを取得

--- laravel-study/app/Http/Controllers/PostController.php
+++ laravel-study/app/Http/Controllers/PostController.php
@@ -22,4 +22,24 @@ class PostController extends Controller
         return view('post.list',
         ['posts' => $posts]);
     }
+
+    /**
+     * 投稿詳細を表示する
+     * @param int $id
+     * @return view
+     */ 
+    public function showDetail($id) {
+        // Eloquent メソッド all
+        $post = Post::find($id);
+
+        // idがない値があった場合 一覧ページに戻る エラーメッセージを表示
+        if (is_null($post)) {
+            // sessionを作成
+            session()->flash('err_msg', 'データがありません。');
+            return redirect(route('posts'));
+        }
+
+        return view('post.detail',
+        ['post' => $post]);
+    }
 }
--- laravel-study/resources/views/post/list.blade.php
+++ laravel-study/resources/views/post/list.blade.php
@@ -8,12 +8,21 @@
 <body>
   <h1>掲示板 投稿一覧</h1>
 
+  <!-- エラーメッセージ -->
+  @if (session('err_msg'))
+    <p style="color: red; font-weight: bold;">{{ session('err_msg') }}</p>
+  @endif
+
   @foreach($posts as $post)
   <tr>
     <td>{{ $post->id }}</td>
     <td>{{ $post->username }}</td>
     <td>{{ $post->comment }}</td>
     <td>{{ $post->postDate }}</td>
+
+    <!-- リンク -->
+    <!-- <p>リンクから詳細ページへ</p>
+    <td><a href="/posts/detail/{{ $post->id }}">{{ $post->id }}</a></td> -->
   </tr>
   @endforeach
 </body>
