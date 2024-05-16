d349c2cd96f606f9196927ec82904338620af468
yuukimochizuki <y.mochizuki.92@gmail.com>
2024-05-16 21:37:53 +0900

    DBからデータ一覧取得

--- laravel-study/resources/views/post/list.blade.php
+++ laravel-study/resources/views/post/list.blade.php
@@ -7,5 +7,14 @@
 </head>
 <body>
   <h1>掲示板 投稿一覧</h1>
+
+  @foreach($posts as $post)
+  <tr>
+    <td>{{ $post->id }}</td>
+    <td>{{ $post->username }}</td>
+    <td>{{ $post->comment }}</td>
+    <td>{{ $post->postDate }}</td>
+  </tr>
+  @endforeach
 </body>
 </html>
